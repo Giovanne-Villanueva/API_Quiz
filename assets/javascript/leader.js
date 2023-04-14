d122d6dd57cf4579df1a1aa1225336ddeff3b006
Giovanne-Villanueva <70661928+Giovanne-Villanueva@users.noreply.github.com>
2023-04-13 18:00:27 -0700

    We added more styling and fixed small errors in the program

--- assets/javascript/leader.js
+++ assets/javascript/leader.js
@@ -1,13 +1,52 @@
 var user= document.querySelector(".user")
 
-function leaderBoard(){
+var players = {
+    length:0,
+    addElem(element){
+        [].push.call(this, element);
+    }
+};
+var order = [];
+var maxObject;
+var picked=false;
+
+console.log(order.length)
+function sortPlayer(){
     var player= JSON.parse(localStorage.getItem("Players"))
-    var info
+
     if(player !== null){
-        for(var object of Array.prototype.values.call(player)){
+        for(var i=0; i<player.length; i++){
+            var maxScore=0;
+            var minTime=0;
+            var j=0;
+            for(var object of Array.prototype.values.call(player)){
+                if( maxScore <= object.score ){
+                    if(minTime<=object.time){
+                        for(var k=0; k<order.length;k++){
+                            if(order[k]===j){
+                                picked=true;
+                                break;
+                            }
+                            else {
+                                picked=false;
+                            }
+                        }
+                        if(!picked){
+                            maxScore=object.score
+                            minTime=object.time
+                            order[i+1]=j;
+                            maxObject=object;
+                        }
+                    }
+                }
+                j++;
+            }
+            maxScore=0;
+            minTime=0;
             info= document.createElement("li")
-            info.textContent=object.name+ " Score: " + object.score + " Time: " + object.time
+            info.textContent=maxObject.name+ " Score: " + maxObject.score + " Time: " + maxObject.time
             user.appendChild(info)
+            j=0;
         }
     }
     else{
@@ -15,5 +54,4 @@ function leaderBoard(){
         info.textContent="No one has played the game yet no score to display"
     }
 }
-
-leaderBoard()
+sortPlayer()
